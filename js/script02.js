ce329749d9707bd716906b8b276805fda06180e0
jbianchi182 <joaobianchip@gmail.com>
2025-04-26 20:23:05 -0300

    creation of script02

---
+++ js/script02.js
@@ -0,0 +1,8 @@
+function gerarTabuada() {
+    const numero = parseint(document.getElementById("numero").value);
+    const resultado = document.getElementById("resultado");
+    resultado.innerHTML = ''; // Limpa o conteúdo anterior
+
+    for (let i = 1; i <= 10; i++) {
+        const item = 
+}
