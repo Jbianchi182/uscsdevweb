758ef14a6842a330bb9e8e2cf5f6bdf89b998ee8
jbianchi182 <joaobianchip@gmail.com>
2025-04-27 03:54:10 -0300

    ex04 e script04

--- js/script04.js
+++ js/script04.js
@@ -1,3 +1,28 @@
 function checarPares(){
-    
+    var numero = parseInt(document.getElementById("par").value, 10);
+    var resultado = document.getElementById("resultado");
+    var soma = numero;
+
+    if (isNaN(numero)) {
+        document.getElementById('resultado').innerHTML = "Por favor, insira um número válido.";
+    }
+    else {
+        if (numero%2===0) {
+            var i = numero;
+                for (i = i-2; i >0 ; i -=2) {
+                    soma += i;
+                }
+      }
+        else {
+            var i = numero-1;
+            soma = soma-1;
+                for (i = i-2; i > 0; i -=2) {
+                soma += i;
+                }
+
+            }
+        resultado = soma;
+        document.getElementById("resultado").innerHTML = "A soma dos numeros pares anteriores ao número " + numero + " é " + resultado;
+
+    } 
 }
